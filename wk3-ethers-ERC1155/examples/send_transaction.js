9c95aef6b51ae553aaa01e180665ccd1fd3f074b
dm4th <danny.mathieson233@gmail.com>
2022-12-05 12:41:13 -0800

    Initial commit for week 3 - Going through ethers.js walkthrough

---
+++ wk3-ethers-ERC1155/examples/send_transaction.js
@@ -0,0 +1,14 @@
+const { ethers } = require("ethers");
+
+const INFURA_ID = '585b243f415f4d08b2599e59e8a15053';
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+
+const my_address = '0x1dA9Fe57d451F1E66B869f5FAa62c926fF723332';
+const dai_address = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI contract from etherscan
+
+
+const main = async () => {
+    
+}
+
+main();
