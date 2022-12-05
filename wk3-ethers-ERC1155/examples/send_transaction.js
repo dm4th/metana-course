ffc9e3fd5fbeead1d2afccdee7f55b390bfc3985
dm4th <danny.mathieson233@gmail.com>
2022-12-05 13:37:05 -0800

    Transactions portion of walkthrough video

--- wk3-ethers-ERC1155/examples/send_transaction.js
+++ wk3-ethers-ERC1155/examples/send_transaction.js
@@ -1,14 +1,38 @@
 const { ethers } = require("ethers");
 
 const INFURA_ID = '585b243f415f4d08b2599e59e8a15053';
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${INFURA_ID}`); // working on test network
 
-const my_address = '0x1dA9Fe57d451F1E66B869f5FAa62c926fF723332';
-const dai_address = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI contract from etherscan
+const account1 = ''; // TEST1 - sender
+const account2 = ''; // TEST2 - receiver
 
+// Private Keys for Test Accounts
+const private1 = '';
+const private2 = '';
+const wallet1 = new ethers.Wallet(private1, provider);
 
 const main = async () => {
-    
+    // show account balances before transfer
+    var bal1 = await provider.getBalance(account1);
+    var bal2 = await provider.getBalance(account2);
+    console.log(`Before\nAccount 1:\t${ethers.utils.formatEther(bal1)}\nAccount 2:\t${ethers.utils.formatEther(bal2)}\n`);
+
+
+    // send ether
+    const tx = await wallet1.sendTransaction({
+        to: account2,
+        value: ethers.utils.parseEther("0.01")
+    });
+
+    // wait for transaction to execute
+    await tx.wait();
+    // fetch transaction
+    console.log(tx);
+
+    // show account balances after transfer
+    bal1 = await provider.getBalance(account1);
+    bal2 = await provider.getBalance(account2);
+    console.log(`\nAccount 1:\t${ethers.utils.formatEther(bal1)}\nAccount 2:\t${ethers.utils.formatEther(bal2)}\n`);
 }
 
 main();
