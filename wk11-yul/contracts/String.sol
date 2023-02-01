01e697c2989092392bb028065c77997be8dacdc7
dm4th <danny.mathieson233@gmail.com>
2023-01-31 19:06:48 -0800

    Got string assignment in week 11 working in Remix - writing tests tomorrow

--- wk11-yul/contracts/String.sol
+++ wk11-yul/contracts/String.sol
@@ -14,15 +14,30 @@ contract String {
 
    function charAt(string memory input, uint index) public pure returns(bytes2) {
 
+        bytes memory output;
+
         assembly{
+            output := mload(0x40)                       // load free memory pointer
+            let stringLength := mload(input)            // returns number of bytes in the input (first byte is the length in bytes)
+            let _input := mload(sub(output, 0x20))      // the input value is going to be one slot before 
+                                                        // the free memory pointer because of how it's passed in to the function
 
-            // add logic here
+            switch lt(stringLength, index)
+            // index is beyond the length of the string, return 0
+            case 1 {
+                mstore(output, 0)
+            }
+            // can return a value
+            default {
+                for { let i := 0}   lt(i, index) { i:= add(i, 1)}   // need to isolate the last word
+                {
+                    _input := mul(_input, 256)                      // shift 2 bytes to the right
+                }
 
-            // return the character from input at the given 
-
-            // index
-
-            // where index is base 0
+                mstore(output, and(_input,0xff00000000000000000000000000000000000000000000000000000000000000))  // just store the first 2 bytes
+            }
+            
+            return(output, 32)
 
         }
 
